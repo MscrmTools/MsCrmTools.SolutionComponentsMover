d01636eff4e2606647a95faa04d93300a5161006
evorio <support@evorio.co.uk>
2020-10-08 21:32:20 +0100

    String and date column sorting on Solution Picker control.

--- MsCrmTools.SolutionComponentsMover/UserControls/SolutionPicker.cs
+++ MsCrmTools.SolutionComponentsMover/UserControls/SolutionPicker.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xrm.Sdk;
 using MsCrmTools.SolutionComponentsMover.AppCode;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
@@ -10,10 +11,13 @@ namespace MsCrmTools.SolutionComponentsMover.UserControls
     public partial class SolutionPicker : UserControl
     {
         private IOrganizationService service;
+        private ListViewColumnSorter lvColumnSorter; 
 
         public SolutionPicker()
         {
             InitializeComponent();
+            lvColumnSorter = new ListViewColumnSorter();
+            this.lvSolutions.ListViewItemSorter = lvColumnSorter;
         }
 
         public event EventHandler<SolutionSelectedEventArgs> OnSolutionSelected;
@@ -25,7 +29,7 @@ namespace MsCrmTools.SolutionComponentsMover.UserControls
             get { return lvSolutions.SelectedItems.Cast<ListViewItem>().Select(i => (Entity)i.Tag).ToList(); }
         }
 
-        public IOrganizationService Service { set { service = value; } }
+        public IOrganizationService Service { set => service = value; }
 
         public void LoadSolutions(IEnumerable<Entity> solutions)
         {
@@ -45,6 +49,7 @@ namespace MsCrmTools.SolutionComponentsMover.UserControls
                 var item = new ListViewItem(solution.GetAttributeValue<string>("friendlyname"));
                 item.SubItems.Add(solution.GetAttributeValue<string>("uniquename"));
                 item.SubItems.Add(solution.GetAttributeValue<EntityReference>("publisherid").Name);
+                item.SubItems.Add(solution.GetAttributeValue<DateTime>("installedon").ToShortDateString());
                 item.Tag = solution;
 
                 list.Add(item);
@@ -58,5 +63,33 @@ namespace MsCrmTools.SolutionComponentsMover.UserControls
             if (lvSolutions.SelectedItems.Count == 1)
                 OnSolutionSelected?.Invoke(this, new SolutionSelectedEventArgs((Entity)lvSolutions.SelectedItems.Cast<ListViewItem>().First().Tag));
         }
+
+        private void LvSolutions_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            // Determine if clicked column is already the column that is being sorted.
+            if (e.Column == lvColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvColumnSorter.SortColumn = e.Column;
+                lvColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.lvSolutions.Sort();
+        }
     }
+
+ 
 }
